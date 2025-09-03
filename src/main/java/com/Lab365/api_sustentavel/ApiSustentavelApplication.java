7db24a7ff6f5d0ca4dfb01ebe7d3410e0bdcc0f5
AdenilsonNegri <adenilsonegri@gmail.com>
2025-09-03 00:19:05 -0300

    Primeiro commit

---
+++ src/main/java/com/Lab365/api_sustentavel/ApiSustentavelApplication.java
@@ -0,0 +1,13 @@
+package com.Lab365.api_sustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSustentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSustentavelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lab365/api_sustentavel/ApiSustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lab365.api_sustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
